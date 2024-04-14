15db49cf89280998635347151bcd2294da7096de
Anthony Saliba <anthonysaliba93@gmail.com>
2024-04-14 16:51:03 +0300

    More github page fixes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <HashRouter>
         <Nav />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="" element={<Home />} />
           <Route path="/properties" element={<Properties />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NoPage />} />
